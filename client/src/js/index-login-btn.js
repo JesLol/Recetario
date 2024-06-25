fd31be84b6bbcaaaea5fa104379e354903b5af69
JesLol <juanesmas50@gmail.com>
2024-06-24 23:13:41 -0600

    Se agrega boton para ir a agregar recetas desde el index junto con sus estilos y modificaciones para mostrar cuando haya sesion activa

--- client/src/js/index-login-btn.js
+++ client/src/js/index-login-btn.js
@@ -1,5 +1,24 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    document.getElementById("login-btn").addEventListener("click",()=>{
-        window.location.href = "/login";
-    })
+    //Revisa si hay cookie de sesion
+    const cookies = document.cookie;
+    const cookiesObj = cookies.split(';').reduce((acc, cookie) => {
+        const [name, value] = cookie.trim().split('=');
+        acc[name] = value;
+        return acc;
+    }, {});
+    const aggRecetaBtn = document.getElementById("agregar-receta-btn");
+    const loginBtn = document.getElementById("login-btn");
+    const miCookie = cookiesObj['jwt'];
+    if(miCookie){
+        aggRecetaBtn.style.display = "block";
+        aggRecetaBtn.addEventListener("click",()=>{
+            window.location.href = "/agregar-recetas";
+        });
+        loginBtn.style.display = "none";
+    }
+    else{
+        loginBtn.addEventListener("click",()=>{
+            window.location.href = "/login";
+        });
+    }
 })
