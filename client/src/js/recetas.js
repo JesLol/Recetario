00bf9838cccc8169aac276cf4440d6e909a8e6dd
JesLol <juanesmas50@gmail.com>
2024-06-27 21:35:27 -0600

    Se agrega feature para mostrar los ingredientes y pasos a partir de la respuesta del fetch

--- client/src/js/recetas.js
+++ client/src/js/recetas.js
@@ -5,12 +5,21 @@ document.addEventListener("DOMContentLoaded",()=>{
     .then(response => response.json())
     .then(data=>{
         recetas = `<ul class="recetas-ul">`;
+        let recetasID = 0;
         data.forEach(receta => {
-            recetas += `<li class="receta-li"><button id="${receta.nombre}" class="li-receta-btn"><p class="receta-nombre">${receta.nombre}</p></button></li>`;
+            recetas += `<li class="receta-li"><button id="${receta.nombre}${recetasID}" class="li-receta-btn"><p class="receta-nombre">${receta.nombre}</p></button></li>`;
+            recetasID++;
         });
         recetas+="</ul>"
-        console.log(recetas)
         recetasContainer.innerHTML = recetas;
+        recetasID = 0;
+        data.forEach(receta => {
+            document.getElementById(`${receta.nombre}${recetasID}`).addEventListener("click",()=>{
+                localStorage.setItem("recetaGet", receta.nombre);
+                window.location.href = "/receta"
+            });
+            recetasID++;
+        });
     })
     .catch(error=>{console.error(`Error en el fetch: ${error}`)})
 })
